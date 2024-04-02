c45023b12e32ba2147d6ce8a8a4854ea514029f8
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-04-02 12:26:58 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tasksContainer = document.querySelector('.tasks-container');
-const completeTasksContainer = document.querySelector('.app-completed');
-const completeOpenButton = document.querySelector('.open-completed-button');
+const completeTasksContainer = document.querySelector('.completed-tasks-container');
+const completeListOpenButton = document.querySelector('.open-completed-button');
 
 const input = document.querySelector('.task-input');
 const addButton = document.querySelector('.task-add-button');
@@ -32,8 +32,11 @@ function createTask(task) {
         checkBox.classList.remove('nocheck');
         checkBox.classList.add('check');
 
-
-    })
+        setTimeout( () => {
+            moveToDoneList(task)
+            tasksContainer.removeChild(container);
+        } ,500);
+    });
 
     removeButton.addEventListener('click', () => {
         tasksContainer.removeChild(container);
@@ -43,9 +46,36 @@ function createTask(task) {
     tasksContainer.appendChild(container);
 }
 
-function moveToDoneList(task) {
+// document.body.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     //completeTasksContainer.removeChild(container);
+// });
 
+function moveToDoneList(task) {
+    const container = document.createElement('div');
+    const removeButton = document.createElement('div');
+    const text = document.createElement('p');
+    const checkMark = document.createElement('div');
+
+    container.classList.add('completed-task');
+    removeButton.classList.add('remove');
+    removeButton.innerHTML = `<svg class="remove" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>`;
+    text.innerHTML = task;
+    checkMark.innerHTML = `<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    </svg>`;
+    checkMark.classList.add('checkmark');
+
+    removeButton.addEventListener('click', () => {
+        completeTasksContainer.removeChild(container);
+    });
+    container.append(removeButton, text, checkMark);
+
+    completeTasksContainer.appendChild(container);
 }
-completeOpenButton.addEventListener('click', () => {
-    completeTasksContainer.classList.toggle('opened');
+
+completeListOpenButton.addEventListener('click', () => {
+    document.querySelector('.app-completed').classList.toggle('opened');
 })
